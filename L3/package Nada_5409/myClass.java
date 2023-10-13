687815957abe7714844403bcc92b8eefd9ee3c83
cokovaan <147845637+cokovaan@users.noreply.github.com>
2023-10-13 18:00:17 +0200

    Create myClass.java

---
+++ L3/package Nada_5409/myClass.java
@@ -0,0 +1,37 @@
+
+package Nada_5409;
+
+public class myNumber {
+	private int num;
+	public int num2;
+	public myNumber() {
+		num=0;
+		num2=100;
+	}
+	int getNum() {
+		return num;
+	}
+	void setNum(int i) {
+		num=i;
+		num2=10*i;
+	}
+	int getNum2() {
+		return num2;
+	}
+	
+void description() {
+	System.out.println("The value of num is" this getNum());
+	System.out.println("The value of num2 is" this getNum2());
+}
+float getAverage() {
+	return (num+num2/2);
+}
+public class myClass {
+	public static void main(String[] args) {
+		myNumber A = new myNumber();
+		System.out.println(A.getNum());
+		A.setNum(10);
+		System.out.println(A.getNum());
+		System.out.println(A.num2);
+	}
+}
